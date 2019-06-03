ad0936e7a145e724de75b339d9c223f7190f88eb
richiello <richiello@hotmail.com>
2019-06-03 16:13:21 +0200

    Add icons for top users

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -9,16 +9,16 @@ import { Injectable } from '@angular/core';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const users = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'Mr. Nice', icon: 'owl' },
+      { id: 12, name: 'Narco', icon: 'fish' },
+      { id: 13, name: 'Bombasto', icon: 'dog' },
+      { id: 14, name: 'Celeritas', icon: 'bird' },
+      { id: 15, name: 'Magneta', icon: 'owl' },
+      { id: 16, name: 'RubberMan', icon: 'owl' },
+      { id: 17, name: 'Dynama', icon: 'owl' },
+      { id: 18, name: 'Dr IQ', icon: 'owl' },
+      { id: 19, name: 'Magma', icon: 'owl' },
+      { id: 20, name: 'Tornado', icon: 'owl' }
     ];
     return { users }
   }
